63b20ac8a2a1d14ad78ecff7b4df2aede1693520
布袋猫 <jinxinliu1997@gmail.com>
2023-01-14 17:07:36 -0500

    add multi input support
    also change the output folder path

--- RangeFilter/Filter_main.m
+++ RangeFilter/Filter_main.m
@@ -1,54 +1,61 @@
 FWT_raw  = xlsread("Dataset\NIV-F-WT.xlsx");
-Input_raw = xlsread("Dataset\NIV-F-L53D.xlsx");
-[filepath,name,ext] = fileparts("Dataset\NIV-F-L53D.xlsx");
 
-% % remove NAN column
-% FWT_raw(: , 6) = [ ]
-% Input_raw(:, 6) = [ ]
+% modify the input file path & name here
+Input_raw_list = [
+    "Dataset\NIV-F-L53D.xlsx",...
+    "Dataset\NIV-F-L53D2.xlsx",...
+    "Dataset\NIV-F-L53D3.xlsx"
+]
 
-% remove NAN Row
-FWT = FWT_raw(~all(isnan(FWT_raw), 2) , :);
-Input = Input_raw(~all(isnan(Input_raw), 2), :);
+% ouput path
+output_path = "D:\GithubRepository\PARA\RangeFilter\Output";
 
-FWT_mean = mean(FWT(:, 16), 1);
-FWT_var = var(FWT(:,16), 0, 1);
+for i=1:length(Input_raw_list)
+    Input_raw_path = Input_raw_list{i};
+    Input_raw = xlsread(Input_raw_path);
+    [filepath,name,ext] = fileparts(Input_raw_path);
 
-
-disp(length(Input))
-% if you want to change the range of threshold, just change the coefficient of FWT_var
-% such as FWT_var * 1.0 >> FWT_var * 1.2
-after_filter = filter_by_std_mean(FWT_mean, FWT_var * 1.0, Input);
-disp(length(after_filter))
-
-title = {'Cell', 'ROI', 'x bottom corner', 'y bottom corner', 'Size of ROI (nm)', 	'Comments',...
-'Percentage of molecules in clusters',	'Average number of molecules per cluster', ...
+    % % remove NAN column
+    % FWT_raw(: , 6) = [ ]
+    % Input_raw(:, 6) = [ ]
+    
+    % remove NAN Row
+    FWT = FWT_raw(~all(isnan(FWT_raw), 2) , :);
+    Input = Input_raw(~all(isnan(Input_raw), 2), :);
+    
+    FWT_mean = mean(FWT(:, 16), 1);
+    FWT_var = var(FWT(:,16), 0, 1);
+    
+    
+    disp(length(Input))
+    % if you want to change the range of threshold, just change the coefficient of FWT_var
+    % such as FWT_var * 1.0 >> FWT_var * 1.2
+    after_filter = filter_by_std_mean(FWT_mean, FWT_var * 1.0, Input);
+    disp(length(after_filter))
+    
+    title = {'Cell', 'ROI', 'x bottom corner', 'y bottom corner', 'Size of ROI (nm)', 	'Comments',...
+    'Percentage of molecules in clusters',	'Average number of molecules per cluster', ...
     'Average cluster area (nm^2)', 'Abslute density in clusters (molecules / um^2)', ...
     'Relative density in clusters', 'Total number of molecules in ROI', 'Circularity', ...
     'Number of clusters in ROI',	'Density of clusters (clusters / um^2)', 'TAD'
-};
-T = array2table(after_filter);
-T.Properties.VariableNames(1:16) = title;
-writetable(T, name+'_after_filter.csv');
+    };
+    T = array2table(after_filter);
+    T.Properties.VariableNames(1:16) = title;
+    str = [string(name) + '_after_filter.csv'];  % in matlab it do not have the function in connect char with string
+    final_out_path = fullfile(output_path, str);
+    writetable(T, final_out_path);
 
-%% plot
-figure(1)
-h1 = histfit(FWT(:, 16), 20);
-hold on
-h2 = histfit(Input(:, 16), 20);
-hold off
+    %% plot
+    figure(1)
+    h1 = histfit(FWT(:, 16), 20);
+    hold on
+    h2 = histfit(Input(:, 16), 20);
+    hold off
+    
+    figure(2)
+    h3 = histogram(after_filter(:, 16), 20);
 
-figure(2)
-h3 = histogram(after_filter(:, 16), 20);
-
-% the sigma is too small for matlab to generate a plot
-% x = (-FWT_mean: 0.0002: FWT_mean*2);
-% mu = FWT_mean;
-% sigma = 0.001;
-% % f = exp(-(y-mu).^2./(2*sigma^2))./(sigma*sqrt(2*pi));
-% y = gaussmf(x, [sigma mu]);
-% plot(x, y)
-
-% plot(y, f , 'LineWidth',1.5)
+end
 
 
 %% below are function define
