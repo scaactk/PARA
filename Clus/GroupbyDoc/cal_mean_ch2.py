8d0c3b938bdd6ca39f30b5ae9fbf9c8523809605
布袋猫 <jinxinliu1997@gmail.com>
2023-05-07 21:18:42 -0400

    add new script only calculate for channel 2

--- Clus/GroupbyDoc/cal_mean_ch2.py
+++ Clus/GroupbyDoc/cal_mean_ch2.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 root_path = r'C:\Users\scaactk\Desktop\test'
 for root, dir, files in os.walk(root_path):
-    mean_channel_1 = pd.DataFrame(columns=["ROI",
-                                           "mean_DoC_for_nonclustered_events",
-                                           "mean_DoC_for_clustered_and_col_events",
-                                           "mean_DoC_for_clustered_and_noncol_events",
-                                           ])
+    # mean_channel_1 = pd.DataFrame(columns=["ROI",
+    #                                        "mean_DoC_for_nonclustered_events",
+    #                                        "mean_DoC_for_clustered_and_col_events",
+    #                                        "mean_DoC_for_clustered_and_noncol_events",
+    #                                        ])
     mean_channel_2 = pd.DataFrame(columns=["ROI",
                                            "mean_DoC_for_nonclustered_events",
                                            "mean_DoC_for_clustered_and_col_events",
@@ -18,8 +18,8 @@ for root, dir, files in os.walk(root_path):
         suffix = item.split('.')[-1]
         if suffix == 'xlsx' and 'statistics' not in name and 'mean' not in name:
             ROI_number = name.split('_')[1]
-            if name.split('_')[-1] == 'Ch1':
-                channel = 1
+            if name.split('_')[-1] == 'Ch1': # only for channel 2
+                continue
             elif name.split('_')[-1] == 'Ch2':
                 channel = 2
             else:
@@ -31,14 +31,14 @@ for root, dir, files in os.walk(root_path):
             sum_of_clustered_and_col_events = 0
             sum_of_clustered_and_noncol_events = 0
 
-            number_of_non_clustered_events = 0
+            number_of_nonclustered_events = 0
             number_of_clustered_and_col_events = 0
             number_of_clustered_and_noncol_events = 0
 
             for row in data.itertuples():
                 if getattr(row, 'ClusterID') == 0:
                     sum_of_nonclustered_events += getattr(row, 'DOC')
-                    number_of_non_clustered_events += 1
+                    number_of_nonclustered_events += 1
                 elif getattr(row, 'ClusterID') != 0:
                     if getattr(row, 'Col') == 1:
                         sum_of_clustered_and_col_events += getattr(row, 'DOC')
@@ -51,7 +51,7 @@ for root, dir, files in os.walk(root_path):
                         exit(-999)
 
             # in case of divided by zero
-            if number_of_non_clustered_events == 0:
+            if number_of_nonclustered_events == 0:
                 number_of_non_clustered_events = 1
             elif number_of_clustered_and_col_events == 0:
                 number_of_clustered_and_col_events = 1
@@ -60,22 +60,22 @@ for root, dir, files in os.walk(root_path):
 
             if channel == 1:
                 mean_channel_1 = mean_channel_1.append({"ROI": ROI_number,
-                                                        "mean_DoC_for_nonclustered_events": sum_of_nonclustered_events / number_of_non_clustered_events,
+                                                        "mean_DoC_for_nonclustered_events": sum_of_nonclustered_events / number_of_nonclustered_events,
                                                         "mean_DoC_for_clustered_and_col_events": sum_of_clustered_and_col_events / number_of_clustered_and_col_events,
                                                         "mean_DoC_for_clustered_and_noncol_events": sum_of_clustered_and_noncol_events / number_of_clustered_and_noncol_events
                                                         }, ignore_index=True)
             elif channel == 2:
                 mean_channel_2 = mean_channel_2.append({"ROI": ROI_number,
-                                                        "mean_DoC_for_nonclustered_events": sum_of_nonclustered_events / number_of_non_clustered_events,
+                                                        "mean_DoC_for_nonclustered_events": sum_of_nonclustered_events / number_of_nonclustered_events,
                                                         "mean_DoC_for_clustered_and_col_events": sum_of_clustered_and_col_events / number_of_clustered_and_col_events,
                                                         "mean_DoC_for_clustered_and_noncol_events": sum_of_clustered_and_noncol_events / number_of_clustered_and_noncol_events
                                                         }, ignore_index=True)
 
-    print(mean_channel_1.head())
+    # print(mean_channel_1.head())
     print(mean_channel_2.head())
-    output_name = root_path + '/' + 'mean.xlsx'
+    output_name = root_path + '/' + 'mean_ch2.xlsx'
     with pd.ExcelWriter(output_name) as writer:
-        mean_channel_1.to_excel(writer, sheet_name='Chan1', index=False)
+        # mean_channel_1.to_excel(writer, sheet_name='Chan1', index=False)
         mean_channel_2.to_excel(writer, sheet_name='Chan2', index=False)
     print("OK")
 
